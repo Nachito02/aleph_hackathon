c77cf6868f9cc76fa80b7e2cf2b9a4ede819c1f1
Nachito02 <arguellojuan08@gmail.com>
2025-03-21 23:43:22 -0300

    add state

--- src/components/MiniProvider.tsx
+++ src/components/MiniProvider.tsx
@@ -1,17 +1,24 @@
-import { useEffect, ReactNode } from "react";
-import { MiniKit } from '@worldcoin/minikit-js'
+import { useEffect, useState, ReactNode } from "react";
+import { MiniKit } from "@worldcoin/minikit-js";
 
-
-export default function MiniKitProvider({children} : {children: ReactNode}) {
+export default function MiniKitProvider({ children }: { children: ReactNode }) {
+    const [statusList, setStatusList] = useState<string[]>([]);
 
     useEffect(() => {
-        MiniKit.install(import.meta.env.VITE_APP_ID)
-    }, [])
+        MiniKit.install(import.meta.env.VITE_APP_ID);
+    }, []);
 
+    const handleClick = () => {
+        setStatusList((prev) => [...prev, MiniKit.isInstalled().toString()]);
+    };
 
-    return <>
-        <h1>MiniKit {MiniKit.isInstalled().toString()}</h1>
-        <button onClick={()=> alert(MiniKit.isInstalled().toString())}>eskere</button>
-        {children}
-    </>
+    return (
+        <>
+            <button onClick={handleClick}>Eskere</button>
+            {statusList.map((status, index) => (
+                <h1 key={index}>MiniKit {status}</h1>
+            ))}
+            {children}
+        </>
+    );
 }
