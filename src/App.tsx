eca22006abd9de9690b2c749384b6f822eee7e8f
naim melita <naimmelita@gmail.com>
2025-03-22 10:16:31 -0300

    landin page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from "./components/Login";
 import Home from "./components/Home";
 import Register from "./components/Register";
+import Account from "./components/Account";
 function App() {
   return (
     <>
@@ -16,7 +17,9 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/cuenta" element={<Account />} />
           </Routes>
+          
           <Footer />
         </Router>
       </MiniKitProvider>
